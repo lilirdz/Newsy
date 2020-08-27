e8ce401702163f744f05b64b868107af3fcc7f6e
lilirdz <lilimrod@gmail.com>
2020-08-26 22:07:51 -0500

    Firebase Setup

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXCI5jH0ushOMCCuj90upCCLpj_6G4Eh4",
+  authDomain: "newsy-7cd3d.firebaseapp.com",
+  databaseURL: "https://newsy-7cd3d.firebaseio.com",
+  projectId: "newsy-7cd3d",
+  storageBucket: "newsy-7cd3d.appspot.com",
+  messagingSenderId: "269606288658",
+  appId: "1:269606288658:web:18b46a6bc6262eb17247cb",
+};
+
+export default firebaseConfig;
